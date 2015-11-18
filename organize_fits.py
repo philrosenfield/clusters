7f262b1920e375a0fce6b823d8ffc84f0faf1135
Phil Rosenfield <philrosenfield@gmail.com>
2015-11-18 09:49:17 -0500

    pulled os.system commands from organize_fits so it writes to a bash script instead.

--- organize_fits.py
+++ organize_fits.py
@@ -1,7 +1,7 @@
 #!/astro/apps6/anaconda2.0/bin/python
 """
-move all fits files in current directory to directory structure based on fits 
-header:
+make a script to move all fits files in current directory to directory structure
+based on its header:
 ./[INSTRUME]/[PROPOSID]_[TARGNAME]/
 
 (will make the directories if they do not exist.)
@@ -15,40 +15,45 @@ from astropy.io import fits
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Move fits files to directory structure based on fits header INSTRUME/PROPOSID_TARGNAME")
-    parser.add_argument("--dryrun", help="do not actually move files or make dirs",
-                        action="store_true")
+    parser.add_argument("-o", "--outfile", type=str, default='organaize.sh',
+                        help="script name to write to")
 
     args = parser.parse_args(argv)
-    create_dirstruct(dryrun=args.dryrun)
+    create_dirstruct(outfile=args.outfile)
 
-def create_dirstruct(dryrun=True):
+def create_dirstruct(outfile=None):
+    line = ''
     fitslist = glob.glob1(os.getcwd(), '*fits')
     if len(fitslist) == 0:
-        print 'nothing to do.'
+        line += 'nothing to do.\n'
     else:
         for fit in fitslist:
             try:
                 hdu = fits.open(fit)
             except:
-                print('problem with %s' % fit)
+                line += '# problem with %s\n' % fit)
                 pass
             header = hdu[0].header
             try:
-   	       newdir = '%i_%s' % (hdu[0].header['PROPOSID'], hdu[0].header['TARGNAME'])
-               newdir = os.path.join(header['INSTRUME'], newdir)
-	    except:
-               print('error in header. skipping {}'.format(fit))
-               continue
-            cmd = 'mv -i %s %s' % (fit, newdir)
+                newdir = '%i_%s' % (hdu[0].header['PROPOSID'], hdu[0].header['TARGNAME'])
+                newdir = os.path.join(header['INSTRUME'], newdir)
+            except:
+                line += 'error in header. skipping {}\n'.format(fit)
+                continue
+
             if not os.path.isdir(newdir):
-                if not dryrun:
-                    os.makedirs(newdir)
-                else:
-                    print 'mkdirs %s' % newdir
-            if not dryrun:
-	        os.system(cmd)
-            else:
-                print cmd
+               line += 'mkdirs %s\n' % newdir
+            cmd = 'mv -i %s %s\n' % (fit, newdir)
+            line += cmd
+
+    if outfile is None:
+        print(line)
+    else:
+        with open(outfile, 'r') as out:
+            out.write(line)
+
 
 if __name__ == "__main__":
+
+
     main(sys.argv[1:])
