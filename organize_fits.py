1efc71930c2b661c91e84f41eb934711f0b043f2
Phil Rosenfield <philrosenfield@gmail.com>
2016-03-15 18:52:21 -0400

    finished v2

--- organize_fits.py
+++ organize_fits.py
@@ -14,6 +14,7 @@ import sys
 from astropy.io import fits
 import numpy as np
 from scipy.spatial import KDTree
+from time import localtime, strftime
 
 def unique2d(a):
     return np.array(list(set(tuple(i) for i in a.tolist())))
@@ -38,7 +39,7 @@ def separate(tablename, radius=0.1):
 
     # Fix names -- check different targets in each group
     # Rename ANY and SFH..
-    
+
 
 def nearest_targets(cluster_table, field_table):
     cradec = np.genfromtxt(cluster_table, usecols=(4,5))
@@ -89,7 +90,7 @@ def commonnames():
         print cmast['target'][i], t['MAIN_ID']
 
 def good_ext(fnames):
-    exts = ['drz', 'flt', 'flc', 'c0m', 'c1m']
+    exts = ['raw', 'flt', 'flc', 'c0m', 'c1m']
     f = np.concatenate([[f for f in fnames if ext in f] for ext in exts])
     if len(f) == 0:
         print('No good extensions found {}, {}'.format(fnames, exts))
@@ -103,40 +104,157 @@ def main(argv):
     args = parser.parse_args(argv)
     create_dirstruct(outfile=args.outfile)
 
+def verify(fitsfileslist):
+     fitslist = map(str.strip, open(fitsfileslist).readlines())
+     for f in fitslist:
+        inst, pidtarg, fname = f.split('/')
+        pid, targ = pidtarg.split('_')
+        hdr = fits.getheader(f)
+
+        if hdr['PROPOSID'] != int(pid):
+            print('{}: {} does not match {}'.format(f, hdr['PROPOSID'], pid))
+        if hdr['targname'] != targ:
+            print('{}: {} does not match {}'.format(f, hdr['targname'], targ))
+
+        if f.endswith('flc'):
+            cal = float(hdr['CAL_VER'][:3])
+            if cal < 3.3:
+                print('{}: CAL_VER is less than 3.3: {}'.format(f, hdr['CAL_VER']))
+
+def checkflcraw():
+    #ipython
+    dirs = !! ls
+    for dir in dirs:
+        ls = os.listdir(dir)
+        flts = [os.path.join(dir, l) for l in ls if 'flt' in l]
+        flcs = [l.replace('flt', 'flc') for l in flts]
+        for f in flcs:
+            if not os.path.isfile(f):
+                raw = f.replace('flc', 'raw')
+                #print '{}: no flc'.format(f.replace('flc', 'flt'))
+                if not os.path.isfile(raw):
+                    print '{}: no raw'.format(f.replace('flc', 'flt'))
+
+    dirs = !! ls
+    for dirt in dirs:
+        filts = []
+        lst = os.listdir(dirt)
+        hdrs = [fits.getheader(os.path.join(dirt,f)) for f in lst]
+        for i in range(len(hdrs)):
+            try:
+                filts.append(hdrs[i]['FILTER'])
+            except:
+                filts.append(hdrs[i]['FILTER1'])
+                filts.append(hdrs[i]['FILTER2'])
+        print len(np.unique(filts)), np.unique(filts), dirt
+
+
+
+
 def same_targ(fits):
     return np.unique([f.split('_')[0] for f in fits], return_index=True)
 
-def create_dirstruct(outfile=None):
-    line = ''
-    fitslist = glob.glob1(os.getcwd(), '*fl*fits')
-    if len(fitslist) == 0:
-        line += 'nothing to do.\n'
-    else:
-        import pdb; pdb.set_trace()
-    fnames, inds = same_targ(fitslist)
-    print('{} fits files'.format(len(fitslist)))
-    for i in range(len(fnames)):
-        if i+1 % 100 == 0:
-            print('{} of {}'.format(i+1, len(fnames)))
-        try:
-            fit = good_ext(fitslist[inds[i]:inds[i+1]])
-            hdu = fits.open(fit)
-            header = hdu[0].header
-        except:
-            line += '# problem with %s\n' % fnames[i]
-            continue
+def update_fits(data, key='targname'):
+    """
+    Update a key of fits files.
 
-        try:
-           newdir = '%i_%s' % (hdu[0].header['PROPOSID'], hdu[0].header['TARGNAME'])
-           newdir = os.path.join(header['INSTRUME'], newdir)
-        except:
-           line += 'error in header. skipping {}\n'.format(fit)
-           continue
-        cmd = 'mv -i %s*fits %s\n' % (fnames[i], newdir)
-        if not os.path.isdir(newdir):
-            os.makedirs('{}'.format(newdir))
-            print('made dir {}'.format(newdir))
-        line += cmd
+    data is a table or an array and needs:
+
+    filename : str
+        local path to fits file
+
+    key : str
+        the new key to replace the key in the header file. (default: targname)
+
+    """
+    fmt = '{}: Updated {} from {} to {}'
+    now = strftime("%Y-%m-%d %H:%M:%S", localtime())
+    for i in range(len(data)):
+        fname = data['filename'][i]
+        fnames = [fname]
+        flc = fname.replace('flt', 'flc')
+        raw = fname.replace('flt', 'raw')
+        if os.path.isfile(flc):
+            fnames.append(flc)
+        if os.path.isfile(raw):
+            fnames.append(raw)
+        for f in fnames:
+            hdu = fits.open(f, mode='update')
+            hdr = hdu[0].header
+            otarg = hdr[key]
+            ntarg = data[key][i]
+            if ntarg != otarg:
+                msg = fmt.format(now, key, otarg, ntarg)
+                hdr[key] = ntarg
+                hdr['history'] = msg
+                print(msg)
+                hdu.flush()
+            else:
+                print('doing nothing for {}: {} {}'.format(f, otarg, ntarg))
+
+#data = pd.read_csv('multiband.csv', header=0)
+#create_dirstruct(outfile='multiband.sh', data=data, propid=13901, basedir='multiband')
+#data = pd.read_csv('final_acs.csv', header=0)
+#create_dirstruct(outfile='acs.sh', data=data)
+#data = pd.read_csv('final_wfc3.csv', header=0)
+#create_dirstruct(outfile='wfc3.sh', data=data)
+
+def create_dirstruct(outfile=None, data=None, propid=None, basedir=None):
+    line = ''
+    if data is None:
+        fitslist = glob.glob1(os.getcwd(), '*fits')
+        fnames, inds = same_targ(fitslist)
+    else:
+        fitslist = data['filename']
+
+    if len(fitslist) == 0:
+        print('nothing to do.')
+        return
+
+    print('{} fits files'.format(len(fitslist)))
+    for i in range(len(fitslist)):
+        fname = fitslist[i]
+        fnames = [fname]
+        flc = fname.replace('flt', 'flc')
+        raw = fname.replace('flt', 'raw')
+        if os.path.isfile(flc):
+            fnames.append(flc)
+        if os.path.isfile(raw):
+            fnames.append(raw)
+        for f in fnames:
+            try:
+                hdu = fits.open(f)
+                hdr = hdu[0].header
+            except:
+
+                if not os.path.isfile(f):
+                    print('{} not found, already moved?'.format(f))
+                else:
+                    line += 'echo "problem with {}"\n'.format(f)
+                continue
+            try:
+                if propid is None:
+                    tpropid = hdr['PROPOSID']
+                else:
+                    tpropid = propid
+
+                if basedir is None:
+                    tbasedir = hdr['INSTRUME']
+                else:
+                    tbasedir = basedir
+
+                newdir = '%i_%s' % (tpropid, hdr['TARGNAME'])
+                newdir = os.path.join(tbasedir, newdir)
+            except:
+                line += 'echo "error in header. skipping {}"\n'.format(f)
+                continue
+
+            line += 'echo "mv -i {} {}:"\n'.format(f, newdir)
+            line += 'mv -i {} {}\n'.format(f, newdir)
+
+            if not os.path.isdir(newdir):
+                os.makedirs('{}'.format(newdir))
+                print('made dir {}'.format(newdir))
 
     if outfile is None:
         print(line)
