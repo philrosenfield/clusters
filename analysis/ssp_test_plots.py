be117c6f17453fb7320adda05770e959d7304d6f
Phil Rosenfield <philrosenfield@gmail.com>
2016-11-14 11:49:00 -0500

    added paper1.csv

--- analysis/ssp_test_plots.py
+++ analysis/ssp_test_plots.py
@@ -27,7 +27,7 @@ def ssp_test(sspfn):
              'vistep': 0.05,
              'logZ': -0.50,
              'sfr': 5e-3,
-             'lage': 10 ** .3127}  # (Msun / yr need to integrate over bin size)
+             'lage': 10 ** .3127}  # (Msun/yr need to integrate over bin size)
     truelage0 = 10 ** .3000  # Gyr
     truelage1 = 10 ** .3254  # Gyr
 
@@ -49,6 +49,7 @@ def ssp_test(sspfn):
         plt.savefig(fignames[i])
     return
 
+
 def cluster_result_plots(sspfns, oned=False, twod=False, onefig=False):
     """corner plot of a big combine scrn output"""
     labelfmt = r'$\rm{{{}}}$'
@@ -56,32 +57,43 @@ def cluster_result_plots(sspfns, oned=False, twod=False, onefig=False):
                   'trueov', 'dav']
 
     # This assures the same order on the plots
-    marg_cols = ['Av', 'IMF', 'dmod', 'lage', 'logZ', 'bf', 'ov']
+    marg_cols = ['Av', 'IMF', 'bf', 'dmod', 'lage', 'logZ', 'ov']
     nssps = len(sspfns)
     ndim = len(marg_cols)
     fig = None
     axs = [None] * nssps
     if onefig:
         fig, axs = plt.subplots(nrows=nssps, ncols=ndim,
-                                figsize=(1 + ndim * 2, 3 * nssps))
+                                figsize=(ndim * 1.4, nssps))
     for i, sspfn in enumerate(sspfns):
         print(sspfn)
-        ssp = SSP(sspfn, gyr=True)
-
-        ssp.check_grid(skip_cols=avoid_list)
-
+        if sspfn.endswith('.csv'):
+            ssp = SSP(sspfn, gyr=True)
+            ssp.check_grid(skip_cols=avoid_list)
+        else:
+            ssp = SSP()
+            ssp.gyr = True
+            ssp.load_posterior(sspfn)
         targ = ssp.name.split('_')[0].upper()
         label = labelfmt.format(targ)
+        ylabel = labelfmt.format(targ)
+        if onefig:
+            label = None
 
         if oned:
             f, raxs = ssp.pdf_plots(marginals=marg_cols, text=label,
-                                    fig=fig, axs=axs[i])
+                                    fig=fig, axs=axs[i], frompost=True)
 
             if not onefig:
                 figname = sspfn.replace('.csv', '_1d.pdf')
                 plt.savefig(figname)
                 plt.close()
-            ssp.posterior.to_csv('{}_post.dat'.format(targ), index=False)
+            else:
+                raxs[-1].set_ylabel(ylabel)
+                raxs[-1].yaxis.set_label_position("right")
+
+            if not sspfn.endswith('.dat'):
+                ssp.write_posterior(filename='{}_post.dat'.format(targ))
         if twod:
             ssp.pdf_plots(marginals=marg_cols, text=label, twod=True,
                           cmap=plt.cm.Reds)
@@ -89,11 +101,15 @@ def cluster_result_plots(sspfns, oned=False, twod=False, onefig=False):
             plt.savefig(figname)
             plt.close()
     if onefig:
-        [ax.axes.set_xlabel('') for ax in raxs]
+        for ax in axs.ravel()[:-1*ndim]:
+            ax.tick_params(labelbottom='off', tickdir='in')
+            ax.axes.set_xlabel('')
+        [ax.axes.set_ylabel('') for ax in axs[:, 0]]
+        fig.text(0.02, 0.5, labelfmt.format('Probability'), ha='center',
+                 va='center', rotation='vertical')
+        fig.subplots_adjust(hspace=0.07, wspace=0.07, right=0.95, top=0.98,
+                            bottom=0.08)
         figname = 'combo_{}_ssps.pdf'.format(nssps)
-        plt.ion()
-        plt.draw()
-        import pdb; pdb.set_trace()
         plt.savefig(figname)
         plt.close()
     return
